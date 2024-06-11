1aec01003fb35173dfd1d96ff9f7bcd7a7d4e97b
bayuaaar12 <akuntv397@gmail.com>
2024-06-11 10:18:13 +0700

    Initial commit LFS
    mp4 lfs

---
+++ Assets/script/Play.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{ 
+    public void infobumi()
+    {
+        SceneManager.LoadScene("infobumi");
+    }
+
+    public void infoBumi()
+    {
+        SceneManager.LoadScene("infoBumi");
+    }
+}
